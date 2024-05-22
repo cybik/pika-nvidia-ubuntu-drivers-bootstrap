bcac903090746e039858d86deb0c0c5ba60996d1
Renaud Lepage <root@cybikbase.com>
2024-05-22 13:44:08 -0700

    clean

--- release.sh
+++ release.sh
@@ -5,6 +5,8 @@ git config --global --add safe.directory '*'
 
 [ $(command -v gh) ] || { apt -y install gh; }
 
+TAG_NO_REF="$(echo "${GH_TAG}" | sed 's/refs\/tags\///')"
+
 for f in $(find archives -type f); do
-    gh release upload "${GH_TAG}" $f
+    gh release upload "${TAG_NO_REF}" $f
 done
